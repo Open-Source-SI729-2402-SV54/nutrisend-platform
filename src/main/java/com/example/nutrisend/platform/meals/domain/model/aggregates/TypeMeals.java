ee5d0da9ec600a0ba4d48c0da597fe00d39ee489
C7leo <149843920+C7leo@users.noreply.github.com>
2024-10-31 15:34:07 -0500

    feat(aggregates): add entity

---
+++ src/main/java/com/example/nutrisend/platform/meals/domain/model/aggregates/TypeMeals.java
@@ -0,0 +1,45 @@
+package com.example.nutrisend.platform.meals.domain.model.aggregates;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class TypeMeals {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "typeID")
+    private List<Meals> meals;
+
+
+    public TypeMeals() {}
+
+    public TypeMeals(String name, List<Meals> meals) {
+        this.name = name;
+        this.meals = meals;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public List<Meals> getMeals() { return meals; }
+
+    public void setMeals(List<Meals> meals) { this.meals = meals; }
+
+}
