c32a2c6dbda1bf63f08ac7f8b3484e7e09fdfe27
C7leo <149843920+C7leo@users.noreply.github.com>
2024-10-31 16:28:54 -0500

    feat(internal): update

--- src/main/java/com/example/nutrisend/platform/meals/internal/MealsCommandServiceImpl.java
+++ src/main/java/com/example/nutrisend/platform/meals/internal/MealsCommandServiceImpl.java
@@ -1,9 +1,13 @@
 package com.example.nutrisend.platform.meals.internal;
 
+import com.example.nutrisend.platform.meals.domain.model.aggregates.CategoryMeals;
 import com.example.nutrisend.platform.meals.domain.model.aggregates.Meals;
+import com.example.nutrisend.platform.meals.domain.model.aggregates.TypeMeals;
 import com.example.nutrisend.platform.meals.domain.model.commands.*;
 import com.example.nutrisend.platform.meals.domain.services.MealsCommandService;
+import com.example.nutrisend.platform.meals.jpa.CategoryMealsRepository;
 import com.example.nutrisend.platform.meals.jpa.MealRepository;
+import com.example.nutrisend.platform.meals.jpa.TypeMealsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,19 +18,27 @@ import java.util.UUID;
 public class MealsCommandServiceImpl implements MealsCommandService {
 
     private final MealRepository mealsRepository;
+    private final TypeMealsRepository typeMealsRepository;
+    private final CategoryMealsRepository categoryMealsRepository;
 
     @Autowired
-    public MealsCommandServiceImpl(MealRepository mealsRepository) {
+    public MealsCommandServiceImpl(MealRepository mealsRepository, TypeMealsRepository typeMealsRepository, CategoryMealsRepository categoryMealsRepository) {
         this.mealsRepository = mealsRepository;
+        this.typeMealsRepository = typeMealsRepository;
+        this.categoryMealsRepository = categoryMealsRepository;
     }
 
     @Override
     public Optional<Meals> handle(CreateMealsCommand command) {
         String mealId = UUID.randomUUID().toString();
 
-        Meals meal = new Meals(command.name(), command.calories(), command.protein(),
-                command.carbohydrates(), command.fats(), command.price(), command.img());
+        TypeMeals type = typeMealsRepository.findById(command.typeID())
+                .orElseThrow(() -> new IllegalArgumentException("TypeMeals not found for ID: " + command.typeID()));
 
+        CategoryMeals category = categoryMealsRepository.findById(command.categoryID())
+                .orElseThrow(() -> new IllegalArgumentException("CategoryMeals not found for ID: " + command.categoryID()));
+
+        Meals meal = new Meals(type, category, command.name(), command.calories(), command.protein(), command.carbohydrates(), command.fats(), command.price(), command.img() );
         mealsRepository.save(meal);
 
         return Optional.of(meal);
