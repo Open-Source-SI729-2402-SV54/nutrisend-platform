140000b2b293c938e0b4d263c1ec757a0d81b4fd
RenzoA <u202113612@upc.edu.pe>
2024-11-01 09:18:32 -0500

    feat:  ScheduleService update

--- src/main/java/com/example/nutrisend/platform/schedule/domain/services/ScheduleService.java
+++ src/main/java/com/example/nutrisend/platform/schedule/domain/services/ScheduleService.java
@@ -4,12 +4,15 @@ import com.example.nutrisend.platform.schedule.domain.model.aggregates.Schedule;
 import com.example.nutrisend.platform.schedule.domain.model.commands.CreateScheduleCommand;
 import com.example.nutrisend.platform.schedule.domain.model.commands.DeleteScheduleCommand;
 import com.example.nutrisend.platform.schedule.domain.model.commands.UpdateScheduleCommand;
-import com.example.nutrisend.platform.schedule.domain.model.queries.GetScheduleQuery;
 
 import java.util.Optional;
 
 public interface ScheduleService {
     Schedule createSchedule(CreateScheduleCommand command);
+
     Optional<Schedule> updateSchedule(UpdateScheduleCommand command);
+
     boolean deleteSchedule(DeleteScheduleCommand command);
+
     Optional<Schedule> getScheduleByUserId(Long userId);
+}
