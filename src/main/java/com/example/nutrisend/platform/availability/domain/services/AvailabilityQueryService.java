412e707cc84de928b44e94f01b75cfd40b12c93b
C7leo <149843920+C7leo@users.noreply.github.com>
2024-11-17 01:54:44 -0500

    feat(notification): update

--- src/main/java/com/example/nutrisend/platform/availability/domain/services/AvailabilityQueryService.java
+++ src/main/java/com/example/nutrisend/platform/availability/domain/services/AvailabilityQueryService.java
@@ -3,7 +3,6 @@ package com.example.nutrisend.platform.availability.domain.services;
 import com.example.nutrisend.platform.availability.domain.model.aggregates.Availability;
 import com.example.nutrisend.platform.availability.domain.model.queries.GetAllAvailabilityQuery;
 import com.example.nutrisend.platform.availability.domain.model.queries.GetAvailabilityByIdQuery;
-
 import java.util.List;
 import java.util.Optional;
 
